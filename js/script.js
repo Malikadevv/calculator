cd12ff7db86e6779c128c6d1b9469219bd4d594c
Malikadevv <malikajyrgalbaeva@gmail.com>
2023-01-26 14:13:11 +0600

    game over snake malika devv

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ const result = document.querySelector('#result'),
     equals = document.querySelector('.equals'),
     clear = document.querySelector('#clear'),
     ce = document.querySelector('#ce');
-let ex = ''; // the expression string to be eval'd
+let ex = '';
 result.innerHTML = '0';
 
 
 
-function clickN() { // when we click on a number
+function clickN() {
     if(!ex || typeof(ex) === 'number' || ex === '0') {
         expression.innerHTML = this.id;
         ex = this.id;
@@ -18,28 +18,29 @@ function clickN() { // when we click on a number
         expression.innerHTML += this.id;
         ex += this.id;
     }
-    result.innerHTML = ex.split(/\/|\*|\+|-|=/).pop();
-    checkLength(result.innerHTML);
-};
+    result.innerHTML = ex.split(/[\/*+\-=]/).pop();
 
-function clickO() { // when we click on an operation
+    checkLength(result.innerHTML);
+}
+
+function clickO() {
     if(!ex) {
         return;
     }
     ex = ex.toString().replace(/=/, '');
-    if (ex.match(/\/|\*|\+|-|=/)) {
+    if (ex.match(/[\/*+\-=]/)) {
         ex = eval(ex).toString();
     }
     expression.innerHTML = expression.innerHTML.replace(/=/, '') + this.id;
     ex += this.id;
     result.innerHTML = this.id;
-};
+}
 
 
 
-Array.from(num).forEach(function(element) { // assign appropriate function to all numbers and operations
+Array.from(num).forEach(function(element) {
     element.addEventListener('click', clickN);
-});
+})
 
 Array.from(operation).forEach(function(element) {
     element.addEventListener('click', clickO);
@@ -61,14 +62,14 @@ ce.addEventListener('click', () => {
         result.innerHTML = 0;
 
         function doCE(arg) {
-            arg = arg.split(/([\/\*\+\-\=])/g);
+            arg = arg.split(/([\/\*+\-\=])/g);
             arg.splice(-1, 1);
             return arg.join('');
         }
     }
 })
 
-// calculate the whole thing
+
 equals.addEventListener('click', ()=> {
     if (!ex) {
         result.innerHTML = '0';
@@ -79,20 +80,20 @@ equals.addEventListener('click', ()=> {
     }
 })
 
-function checkLength(arg) { // if we enter a number that's too long
-    if (arg.toString().length > 14) {
+function checkLength(arg) {
+    if (arg.toString().length > 13) {
         expression.innerHTML = 'number too long'.toUpperCase();
         result.innerHTML = '0';
         ex = '0';
     }
 }
 
-function trim12(arg) { // if we calculate a number that's too long
+function trim12(arg) {
     if (arg.toString().length > 14) {
         ex = parseFloat(arg.toPrecision(12));
         if (ex.toString().length > 14) {
             ex = ex.toExponential(9);
-        };
+        }
         return ex;
     } else {
         return arg;
